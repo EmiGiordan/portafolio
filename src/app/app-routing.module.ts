c021174f25b5906f50d894fbd1c94347b89d7650
Emiliano Giordano <emigiorda@hotmail.com>
2022-10-21 21:37:28 -0300

    routes-creation
    Routes added, .test erased

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { Item1Component } from './pages/item1/item1.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component : PortafolioComponent },
+  { path : 'about', component : AboutComponent },
+  { path : 'item1', component : Item1Component},
+  { path : '**', pathMatch : 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot( routes, { useHash : true } ) ],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { Item1Component } from './pages/item1/item1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    Item1Component
   ],
   imports: [
     BrowserModule,
